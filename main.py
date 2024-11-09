7d84be8f0bf8f4be2b551c17d0cf9e8181ceeaef
00018861 <00018861wiut.uz@gmail.com>
2024-11-10 00:54:48 +0500

    first commit

--- main.py
+++ main.py
@@ -7,11 +7,19 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def create_timetable ():
+    timetable = {
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": []
+    }
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# See PyCharm help at https://www.jetbrains.com/help/py
 
