1ad68da417e7dcca76a0e38f2ddb1a8105848238
00018861 <00018861wiut.uz@gmail.com>
2024-11-12 16:46:00 +0500

    Add files via upload
    version 1

---
+++ csfun_1.py
@@ -0,0 +1,26 @@
+def main():
+  
+  while True:
+    print("1. Add student")
+    print("2. Add date")
+    print("3. Exit the program")
+
+    choice = input("Enter your choice between (1-3): ")
+
+def mark_attendance():
+    #id is preferable because several students can have identical names
+  
+    student_id = input("Enter the student ID: ")
+    while
+    if student_id in student_data:
+        while True
+        print ("Attendance marked for the student {student_id}")
+    else: 
+       print ("ID not found")
+         
+    student_id = input("ID did not found ")
+          
+student_data = {}
+
+def add_date
+# teacher can type date
